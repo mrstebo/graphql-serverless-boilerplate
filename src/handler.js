8c071aa2bfad29aecac0f566493b79e9a2e3a979
Steven Atkinson <steven.atkinson@gmx.com>
2019-05-16 10:07:13 +0100

    Using LokiJS to persist data so we can test add, update and delete.

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,26 @@
 const { ApolloServer } = require("apollo-server-lambda");
+const Loki = require("lokijs");
 const resolvers = require("./resolvers");
 const typeDefs = require("./typeDefs");
+
+const db = new Loki("loki.json");
+
+const loadDatabase = () => new Promise((resolve, reject) => {
+  db.loadDatabase({}, (err) => {
+    if (err) return reject(err);
+    return resolve();
+  });
+});
 
 const server = new ApolloServer({
   resolvers,
   typeDefs,
+  context: async () => {
+    await loadDatabase();
+    return {
+      Products: db.getCollection("Products"),
+    };
+  },
   playground: {
     settings: {
       "request.credentials": "same-origin",
@@ -22,6 +38,8 @@ const handler = server.createHandler({
 module.exports = (event, context) => new Promise((resolve, reject) => {
   handler(event, context, (err, data) => {
     if (err) return reject(err);
-    return resolve(data);
+    db.saveDatabase(() => {
+      return resolve(data);
+    });
   });
 });
